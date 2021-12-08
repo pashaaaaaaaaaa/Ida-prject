ecb5dc51deefcc11c839e4e4fbde4d73a23f31f6
unknown <you@example.com>
2021-12-08 16:58:53 +0300

    pro version

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-main.js
+
+let btn = document.querySelector('.add-btn')
+let cards = document.querySelector('.cards')
+let deleted = document.querySelector('.deleted')
+
+
+btn.addEventListener('click' , () => {
+    cards.style.display = 'block'
+})
+
+deleted.addEventListener('click', () => {
+    console.log(deleted)
+
+})
